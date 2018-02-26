fb275d58c39bb5537d507094318bd9de8cc3d874
[git820373671] <[820373671@qq.com]>
2018-02-26 16:57:29 +0800

    提交保洁页面

---
+++ app/src/components/cleaning/glassDetail.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+    </div>
+</template>
+<script>
+    export default{
+        data: function () {
+            return {
+                msg: '详情玻璃'
+            }
+        }
+    }
+</script>
---
+++ app/src/components/cleaning/smallClassification.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+    </div>
+</template>
+<script>
+    export default{
+        data: function () {
+            return {
+                msg: '保洁分类'
+            }
+        }
+    }
+</script>
