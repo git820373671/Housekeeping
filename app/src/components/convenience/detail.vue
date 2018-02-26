3c2bdaf3d90b2a25e3e83b0836e616f61181ed98
[git820373671] <[820373671@qq.com]>
2018-02-26 16:10:45 +0800

    提交便民信息

--- app/src/components/convenience/detail.vue
+++ app/src/components/convenience/detail.vue
@@ -1,5 +1,25 @@
 <template>
-  <div>
+  <div class="h100 convenience-detail">
+    <div class="panel">
+      <div class="title">招24小时住家保姆，钟点工，育儿嫂等，电话联 系</div>
+      <div class="time">发布时间：2017.04.17</div>
+      <div class="address"><span>服务区域</span>
+        <p>和平</p></div>
+      <div class="link"><span>联系人</span>
+        <p>随老师<br/>15812341234</p>
+        <img src="../../assets/images/tel1.png" class="icon"/>
+      </div>
+    </div>
+    <div class="panel">
+      <div class="panel-title">详细介绍</div>
+      <div class="detail">
+        本人需要招聘保姆，要求会做多种菜系，可以住家，不能打呼噜，人品好本人需要招聘保姆，要求会做多种菜系，可以住家，不能打呼噜，人品好本人需要招聘保姆，要求会做多种菜系，可以住家，不能打呼噜，人品好本人需要招聘保姆，要求会做多种菜系，可以住家，不能打呼噜，人品好本人需要招聘保姆，要求会做多种菜系，可以住家，不能打呼噜，人品好本人需要招聘保姆，要求会做多种菜系，可以住家，不能打呼噜，人品好本人需要招聘保姆，要求会做多种菜系，可以住家，不能打呼噜，人品好。
+      </div>
+    </div>
+    <div class="detail-bottom">
+      <div class="tel">随老师<br/>15812341234</div>
+      <div class="do-tel"><img src="../../assets/images/tel1.png"/>拨打电话</div>
+    </div>
   </div>
 </template>
 <script>
--- app/src/components/convenience/list.vue
+++ app/src/components/convenience/list.vue
@@ -1,12 +1,76 @@
 <template>
-  <div>
+  <div class="h100 convenience">
+    <div class="convenience-tab-box">
+      <ul class="convenience-tab" id="selectBox">
+        <li v-for="(item,index) in tabData" v-on:click="doList(item.id)" :id="item.id">
+          <span>{{item.title}}</span>
+        </li>
+      </ul>
+    </div>
+    <ul class="list clear">
+      <li v-for="(item,index) in listData" class="clear">
+        <router-link :to="{name: 'ConvenienceDetail', params: {id: item.id}}">
+          <div class="badge">{{item.badge}}</div>
+          <div class="title">{{item.title}}<p class="time">{{item.time}}</p></div>
+          <div class="price">
+            {{item.price}}元/月<p class="address">{{item.address}}</p>
+          </div>
+          <div class="name">
+            {{item.name}}
+          </div>
+          <div class="tel">
+            <img src="../../assets/images/tel1.png"/>拨打电话
+          </div>
+        </router-link>
+      </li>
+    </ul>
   </div>
 </template>
 <script>
   export default{
     data: function () {
       return {
-        msg: 'hello vue'
+        listData: [],
+        tabData: [],
+        sum: ''
+      }
+    },
+    created: function () {
+      this.getTab()
+    },
+    methods: {
+      getTab: function () {
+        var url = '/static/data/Convenience/release2.json'
+        var prams = {}
+        var $this = this
+        this.$ajax_get(url, prams, function (data) {
+          if (!data.success) return
+          $this.tabData = data.listData
+          $this.sum = data.listData.length
+          $this.getList($this.tabData[0].id)
+        })
+      },
+      getList: function (type) {
+        var url = '/static/data/Convenience/ConvenienceList.json'
+        var prams = {
+          type: type
+        }
+        var $this = this
+        this.$ajax_get(url, prams, function (data) {
+          if (!data.success) return
+          $this.listData = data.listData
+          var $elC = document.getElementById('selectBox').getElementsByClassName('active')
+          for (var i = 0; i < $elC.length; i++) {
+            $elC[i].setAttribute('class', '')
+          }
+          var $el = document.getElementById(type)
+          $el.setAttribute('class', 'active')
+          console.log($this.sum)
+          document.getElementById('selectBox').style.width = $this.sum * 70 + 'px'
+        })
+      },
+      doList: function (type) {
+        this.getList(type)
       }
     }
   }
