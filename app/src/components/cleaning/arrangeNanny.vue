4c1a9d290f3f639e0e950798e238fdb6295b41b1
[git820373671] <[820373671@qq.com]>
2018-02-27 09:44:55 +0800

    提交保洁页面

---
+++ app/src/components/cleaning/arrangeNanny.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+    </div>
+</template>
+<script>
+    export default{
+        data: function () {
+            return {
+                msg: '保姆预订'
+            }
+        }
+    }
+</script>
---
+++ app/src/components/cleaning/submitRefrigerator.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+    </div>
+</template>
+<script>
+    export default{
+        data: function () {
+            return {
+                msg: '提交冰箱'
+            }
+        }
+    }
+</script>
